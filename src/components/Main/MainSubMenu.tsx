e8d6eb98a569bdfdd78a912b1d5561f092b43114
Lee tae hoon <sper456@naver.com>
2022-09-15 02:37:00 +0900

    Design : main  퍼블작업중

--- src/components/Main/MainSubMenu.tsx
+++ src/components/Main/MainSubMenu.tsx
@@ -1,9 +1,47 @@
 import React from "react";
 import styled from "styled-components";
+import {
+  mainsub_sharebook,
+  mainsub_myVoca,
+  mainsub_community,
+  mainsub_csceneter,
+  mainsub_game,
+} from "../../images/index";
 const MainSubMenu = () => {
   return (
     <>
-      <SubMenu>서브메뉴</SubMenu>
+      <SubMenu>
+        <div>
+          <span>
+            <img src={mainsub_myVoca} />
+          </span>
+          <p>나의 단어장</p>
+        </div>
+        <div>
+          <span>
+            <img src={mainsub_sharebook} />
+          </span>
+          <p>공유 단어장</p>
+        </div>
+        <div>
+          <span>
+            <img src={mainsub_game} />
+          </span>
+          <p>게임</p>
+        </div>
+        <div>
+          <span>
+            <img src={mainsub_community} />
+          </span>
+          <p>자유 게시판</p>
+        </div>
+        <div>
+          <span>
+            <img src={mainsub_csceneter} />
+          </span>
+          <p>고객 지원</p>
+        </div>
+      </SubMenu>
     </>
   );
 };
@@ -13,8 +51,26 @@ export default MainSubMenu;
 const SubMenu = styled.div`
   max-width: 1280px;
   width: 100%;
-  height: 220px;
+  height: 232px;
   margin: 0 auto;
-  background-color: #e7e7e7;
-  margin-top: -170px;
+  background-color: #eff9fc;
+  display: flex;
+  justify-content: space-evenly;
+  position: relative;
+  bottom: 10rem;
+  align-items: center;
+  span,
+  p {
+    display: flex;
+    justify-content: center;
+    cursor: pointer;
+  }
+  p {
+    margin-top: 22px;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 26px;
+    letter-spacing: -0.07em;
+    color: #666666;
+  }
 `;
--- src/components/Main/WordChart.tsx
+++ src/components/Main/WordChart.tsx
@@ -3,7 +3,15 @@ import React from "react";
 const WordChart = () => {
   return (
     <div>
-      <div>box</div>
+      <div> 차트 공간 </div>
+      <span> 토익 100개</span>
+      <span> 토익 100개</span>
+      <span> 토익 100개</span>
+      <span> 토익 100개</span>
+      <span> 토익 100개</span>
+      <span> 토익 100개</span>
+      <span> 토익 100개</span>
+      <span> 토익 100개</span>
     </div>
   );
 };
