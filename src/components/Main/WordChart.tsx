8d38bbad743bd485e22c9c0081a20757da69d521
Lee tae hoon <sper456@naver.com>
2022-09-19 21:39:25 +0900

    feat: 차트 기능 구현

--- src/components/Main/WordChart.tsx
+++ src/components/Main/WordChart.tsx
@@ -1,19 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ReactApexChart from "react-apexcharts";
+import { ApexOptions } from "apexcharts";
+import styled from "styled-components";
+
+const data = [
+  { categoryName: "토익", count: 232 },
+  {
+    categoryName: "토플",
+    count: 112,
+  },
+  {
+    categoryName: "영어",
+    count: 128,
+  },
+];
 
 const WordChart = () => {
+  const [category, setCategory] = useState<string[]>([]);
+  const [series, setSeries] = useState<number[]>([]);
+
+  useEffect(() => {
+    for (const x of data) {
+      setCategory(category => category.concat(x.categoryName));
+      setSeries(series => series.concat(x.count));
+    }
+    return () => {
+      setCategory([]);
+      setSeries([]);
+    };
+  }, []);
+  console.log(category, series);
+  const options: ApexOptions = {
+    chart: {
+      type: "donut",
+    },
+    series: series,
+    labels: category,
+    plotOptions: {
+      pie: {
+        expandOnClick: false,
+      },
+    },
+    colors: ["#2B98BE", "#00B4DB", "#AEE3EE", "#00819D", "#5CD5FF", "#CFE4E9"],
+  };
+
   return (
     <div>
-      <div> 차트 공간 </div>
-      <span> 토익 100개</span>
-      <span> 토익 100개</span>
-      <span> 토익 100개</span>
-      <span> 토익 100개</span>
-      <span> 토익 100개</span>
-      <span> 토익 100개</span>
-      <span> 토익 100개</span>
-      <span> 토익 100개</span>
+      <div id="chart">
+        <ReactApexChart
+          options={options}
+          series={options.series}
+          type="donut"
+          width="500"
+        />
+      </div>
     </div>
   );
 };
 
 export default WordChart;
+
+const ChartBox = styled.div`
+  
+`
