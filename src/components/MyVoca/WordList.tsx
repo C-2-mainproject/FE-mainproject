55c7ba5fed579fdfad15148f8fc643dcbd611b93
SoJaeBeom <sojb0826@gmail.com>
2022-09-26 21:38:34 +0900

    Feat: 나의 단어장 관리의 추가된 단어가 없을때 가이드 박스 추가

--- src/components/MyVoca/WordList.tsx
+++ src/components/MyVoca/WordList.tsx
@@ -40,7 +40,16 @@ const WordList = () => {
     );
   });
 
-  return <WordListLayout>{words}</WordListLayout>;
+  return updateWordList.words.length === 0 ? (
+    <Empty>
+      <h1>영어 단어를 추가해주세요! 👀</h1>
+      <h2>
+        우측 상단 '단어 추가하기'를 클릭하여 간편하게 영어 단어를 추가해보세요!
+      </h2>
+    </Empty>
+  ) : (
+    <WordListLayout>{words}</WordListLayout>
+  );
 };
 
 const WordListLayout = styled.div`
@@ -49,5 +58,32 @@ const WordListLayout = styled.div`
   flex-direction: row;
   flex-wrap: wrap;
   width: 100%;
+`;
+
+const Empty = styled.div`
+  width: 1280px;
+  height: 300px;
+  background: #e4f5fa;
+  margin-top: 75px;
+  margin-bottom: 200px;
+  padding-top: 110px;
+
+  h1 {
+    font-style: normal;
+    font-weight: 700;
+    font-size: 30px;
+    line-height: 43px;
+
+    color: #000000;
+  }
+
+  h2 {
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 26px;
+
+    color: #666666;
+  }
 `;
 export default WordList;
