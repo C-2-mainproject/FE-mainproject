edfabc9bf0f3d46a30329de0d09bad1aeaac51c5
SoJaeBeom <sojb0826@gmail.com>
2022-09-30 21:00:48 +0900

    Feat: 마이페이지 유저 정보 조회 추가

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,6 +1,9 @@
+import axios from "axios";
 import { useEffect } from "react";
 import styled from "styled-components";
+import { MyInfo, MyPageHeader } from "../components";
 import { __getUserInfo } from "../redux/modules/userInfoSlice";
+import { getSessionId } from "../shared/Cookie";
 import { useAppDispatch, useAppSelector } from "../shared/reduxHooks";
 
 const MyPage = () => {
@@ -11,18 +14,57 @@ const MyPage = () => {
     dispatch(__getUserInfo());
   }, []);
 
+  const test1 = async () => {
+    console.log("test1");
+    try {
+      const data = await axios.post("http://newlno.com/api/user/a", {
+        withCredentials: true,
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const test2 = async () => {
+    console.log("test2");
+    try {
+      const data = await axios.get("http://newlno.com/api/user/b", {
+        withCredentials: true,
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const test3 = async () => {
+    console.log("test3");
+    try {
+      const data = await axios.post("http://newlno.com/api/user", {
+        withCredentials: true,
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const test4 = () => {
+    console.log(getSessionId());
+  };
+
   return (
     <MyPageLayout>
       <MyPageWrapper>
         <MyPageBox>
-          <div>
-            <p>마이페이지</p>
-          </div>
+          <MyPageHeader />
+          <MyInfo />
 
-          <div>
-            <img src={userInfo.profileImage} />
-            <p>{userInfo.nickname}</p>
-          </div>
+          <button onClick={test1}>테스트1</button>
+          <button onClick={test2}>테스트2</button>
+          <button onClick={test3}>테스트3</button>
+          <button onClick={test4}>테스트4</button>
         </MyPageBox>
       </MyPageWrapper>
     </MyPageLayout>
@@ -38,17 +80,15 @@ const MyPageLayout = styled.div`
 const MyPageWrapper = styled.div`
   width: 1360px;
   margin: auto;
-  background-color: gray;
 `;
 
 const MyPageBox = styled.div`
   width: 1280px;
   margin: auto;
-  background-color: white;
 
-  div:nth-child(2n) {
-    display: flex;
-  }
+  // div:nth-child(2n) {
+  //   display: flex;
+  // }
 `;
 
 export default MyPage;
